e88e744e40fc6323d733953b24842f9348ecc582
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2018-11-23 12:20:56 +0000

    added core classes

---
+++ source/Company.java
@@ -0,0 +1,11 @@
+public class Company {
+
+  private Staff[] staffMembers;
+  private Holiday[] destinations;
+  public Company(Staff[] staffMembers, Holiday destinations){
+    this.staffMembers;
+    this.destinations;
+  }
+
+  
+}
---
+++ source/W11Pratical.java
@@ -0,0 +1,18 @@
+public class W11Practical {
+
+  public static void main(String[] args){
+    Staff[] staffList  = generateStaffMembers();
+    Holiday[] holidays = generateHolidays(staff);
+    Company company = new Company(staff, holidays);
+  }
+
+  public static Staff[] generateStaffMembers(StaffList, holidays){
+
+    return staffList;
+  }
+
+  public static Holiday[] generateHolidays(Staff[] staff){
+
+    return holidayList;
+  }
+}
