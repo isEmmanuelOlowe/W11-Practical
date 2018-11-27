a9493e054c6c2ccade1ee597fad9da2c8d21df24
eo32 <eo32@pc2-005-l.cs.st-andrews.ac.uk>
2018-11-27 15:58:33 +0000

    Array to ArrayList

--- source/Company.java
+++ source/Company.java
@@ -1,11 +1,11 @@
 public class Company {
 
-  private Staff[] staffMembers;
-  private Holiday[] destinations;
+  private ArrayList<Staff> staffMembers;
+  private ArrayList<Holiday> destinations;
   public Company(Staff[] staffMembers, Holiday destinations){
-    this.staffMembers;
-    this.destinations;
+    this.staffMembers = new ArrayList<Staff>(Arrays.asList(staffMembers));
+    this.destinations = new ArrayList<Staff>(Arrays.asList(destinations));
   }
 
-  
+
 }
